5c0c382d996ced213059bbad3e55ecb23e88546f
Peter Chen <peterchen4321@icloud.com>
2024-02-24 10:32:55 -0500

    abstraction over buffers, incorrect rendering though

--- rustboy-core/src/ppu.rs
+++ rustboy-core/src/ppu.rs
@@ -13,8 +13,20 @@ const HBLANK_END: u32 = 455;
 const SCY_ADDR: u16 = 0xFF42;
 const SCX_ADDR: u16 = 0xFF43;
 const LCDC_ADDR: u16 = 0xFF40;
-const SCREEN_LEN: u32 = 256;
-const SCREEN_SIZE: u32 = 256 * 256;
+
+const MAP_PIXEL_LEN: u32 = 256;
+const MAP_PIXEL_SIZE: u32 = 256 * 256;
+const MAP_WIDTH: u16 = 32;
+const TILE_WIDTH:u16 = 8;
+const TILE_BYTES:u16 = 16;
+const BLOCK_ZERO: u16 = 0x8000;
+const BLOCK_ONE: u16 = 0x8800;
+const BLOCK_TWO: u16 = 0x9000;
+const PALETTE_ADDR:u16 = 0xFF47;
+const WHITE: u32 = 0xff000000;
+const LIGHT_GREY: u32 = 0xff555555;
+const DARK_GREY: u32 = 0xffaaaaaa;
+const BLACK: u32 = 0xffffffff;
 bitflags! {
     struct LCDC: u8 {
         const LCD_ENABLE = 1 << 7;
@@ -39,7 +51,57 @@ enum Mode {
     HBlank,
     VBlank,
 }
+struct Buffer {
+    pub length: u32,
+    pub width: u32,
+    pub data: Vec<u32>,
+}
+impl Buffer {
+    pub fn init(length:u32, width:u32) -> Buffer {
+        let mut temp = Vec::new();
+        temp.resize(length as usize * width as usize, 0);
+        Buffer {
+            length,
+            width,
+            data: temp,
+        }
+    }
+    fn set_pixel(&mut self, y: u8, x: u8, val: u32) {
+        self.data[(y as u32 * self.length + x as u32) as usize] = val;
+    }
+    fn write_tile(&mut self, y: u8, x: u8, palette:u8, tile: &[u16; 8]) {
+        //write to the corresponding values in the buffer
+        for y0 in 0..TILE_WIDTH {
+            for x0 in 0..TILE_WIDTH {
+                //bit number goes right to left not left to right
+                let x1 = TILE_WIDTH - x0 - 1;
+                let palette_ind = (tile[y0 as usize] & (0x03 << x1 * 2)) >> x1 * 2;
+                debug_assert!(palette_ind <= 3);
+                //read the palette's value at the 2 bit palette_ind
+                let color_ind = (palette & (0x03 << palette_ind * 2)) >> palette_ind * 2;
+                let color = match color_ind {
+                    0b00 => WHITE,
+                    0b01 => LIGHT_GREY,
+                    0b10 => DARK_GREY,
+                    0b11 => BLACK,
+                    _ => unreachable!()
+                };
+                let y_loc = y*(TILE_WIDTH as u8) + y0 as u8;
+                let x_loc = x*(TILE_WIDTH as u8) + x0 as u8;
+                self.set_pixel(y_loc, x_loc, color);
+            }
+        }
+        println!("{y}, {x}");
+    }
+}
 impl PPU {
+    pub fn init(bus: Rc<RefCell<dyn Mem>>) -> PPU {
+        PPU {
+            dots: 0,
+            bus,
+            mode: Mode::Search,
+        }
+    }
     //advance the PPU by n CPU clocks, n*4 dots/t cycles
     pub fn tick(&mut self, clocks: u8) {
         self.dots += clocks as u32;
@@ -95,19 +157,25 @@ impl PPU {
             _ => unreachable!(),
         }
     }
-    pub fn calculate_bg_tilemap(&self) -> [u32; SCREEN_SIZE as usize] {
-        const MAP_WIDTH: u16 = 32;
-        const TILE_WIDTH:u16 = 8;
-        const TILE_BYTES:u16 = 16;
-        const BLOCK_ZERO: u16 = 0x8000;
-        const BLOCK_ONE: u16 = 0x8800;
-        const BLOCK_TWO: u16 = 0x9000;
-        const PALETTE_ADDR:u16 = 0xFF47;
-        const WHITE: u32 = 0xff000000;
-        const LIGHT_GREY: u32 = 0xff555555;
-        const DARK_GREY: u32 = 0xffaaaaaa;
-        const BLACK: u32 = 0xffffffff;
-        let mut out = [0u32; SCREEN_SIZE as usize];
+    pub fn debug_tiles(&self) -> [u32; 8 * 8 * 384] {
+        todo!();
+        //let mut out = [0; 8 * 8 * 384];
+        //let bus = self.bus.borrow();
+        ////$8000 - $97FF
+        //let mut i:u16 = 0x8000;
+        //while i <= 0x97FF {
+        //    let mut merged:[u16; 8] = [0; 8];
+        //    for k in 0..8 {
+        //        merged[k as usize] = spread(bus.read(i + 2*k)) | ((spread(bus.read(i + 2*k + 1)) << 1));
+        //    }
+        //    let iterations:u16 = (i - 0x8000) / 16;
+        //    write_tile((iterations/16) as u8, (iterations % 16) as u8, 0b11100100, &merged, &mut out[..]);
+        //    i += 16;
+        //}
+        //out
+    }
+    pub fn calculate_bg_tilemap(&self) -> [u32; MAP_PIXEL_SIZE as usize] {
+        let mut buffer = Buffer::init(MAP_PIXEL_LEN, MAP_PIXEL_LEN);
         let bus = self.bus.borrow();
         //I'm just gonna calculate the whole tile map for now
         let lcdc = LCDC::from_bits(bus.read(LCDC_ADDR)).unwrap();
@@ -138,29 +206,11 @@ impl PPU {
                     let byte_two = bus.read(tile_loc + 2 * i as u16 + 1);
                     merged[i] = spread(byte_one) | (spread(byte_two) << 1);
                 }
-                //write to the corresponding values in the buffer
-                for y0 in 0..TILE_WIDTH {
-                    for x0 in 0..TILE_WIDTH {
-                        let x0 = TILE_WIDTH - x0;
-                        let palette_ind = (merged[y0 as usize] & (0x03 << x0 * 2)) >> x0 * 2;
-                        debug_assert!(palette_ind <= 3);
-                        //read the palette's value at the 2 bit palette_ind
-                        let palette = bus.read(PALETTE_ADDR);
-                        let color_ind = (palette & (0x03 << palette_ind * 2)) >> palette_ind * 2;
-                        let color = match color_ind {
-                            0b00 => WHITE,
-                            0b01 => LIGHT_GREY,
-                            0b10 => DARK_GREY,
-                            0b11 => BLACK,
-                            _ => unreachable!()
-                        };
-                        set_pixel((y*TILE_WIDTH + y0).try_into().unwrap(), 
-                            (x*TILE_WIDTH + x0).try_into().unwrap(), color, &mut out);
-                    }
-                }
+                let palette = bus.read(PALETTE_ADDR);
+                buffer.write_tile(y.try_into().unwrap(), x.try_into().unwrap(), palette, &merged);
             }
         }
-        out
+        buffer.data.try_into().expect("wrong size.")
     }
 }
 fn spread(val: u8) -> u16 {
@@ -171,7 +221,4 @@ fn spread(val: u8) -> u16 {
         }
     }
     out
-}
-fn set_pixel(y: u8, x: u8, val: u32, buffer: &mut [u32; SCREEN_SIZE as usize]) {
-    buffer[(y as u32 * SCREEN_LEN + x as u32) as usize] = val;
 }
