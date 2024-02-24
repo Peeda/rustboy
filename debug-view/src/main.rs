66d72eb1b6aa8bc21ae06bd9075d02ccf9c556bd
Peter Chen <peterchen4321@icloud.com>
2024-02-24 16:39:18 -0500

    moved stuff around in layout

--- debug-view/src/main.rs
+++ debug-view/src/main.rs
@@ -18,17 +18,8 @@ fn main() {
     mem.write(0xff47, 0b00011011);
     let bus = Rc::new(RefCell::new(mem));
     let ppu = PPU::init(bus);
-    let buffer = ppu.calculate_tilemap(true);
 
-    let tex1 = {
-        let img = Image::gen_image_color(256, 256, Color::PURPLE);
-        let mut tex = rl.load_texture_from_image(&thread, &img).unwrap();
-        unsafe {
-            tex.update_texture(&std::mem::transmute::<[u32;65536], [u8;262144]>(buffer));
-        }
-        tex
-    };
-    let debug_tiles = {
+    let tile_data = {
         let img = Image::gen_image_color(24*8, 16*8, Color::PURPLE);
         let mut tex = rl.load_texture_from_image(&thread, &img).unwrap();
         let tile_buffer = ppu.debug_tiles();
@@ -37,11 +28,30 @@ fn main() {
         }
         tex
     };
+    let bg_map = {
+        let buffer = ppu.calculate_tilemap(true);
+        let img = Image::gen_image_color(256, 256, Color::PURPLE);
+        let mut tex = rl.load_texture_from_image(&thread, &img).unwrap();
+        unsafe {
+            tex.update_texture(&std::mem::transmute::<[u32;65536], [u8;262144]>(buffer));
+        }
+        tex
+    };
+    let window = {
+        let buffer = ppu.calculate_tilemap(false);
+        let img = Image::gen_image_color(256, 256, Color::PURPLE);
+        let mut tex = rl.load_texture_from_image(&thread, &img).unwrap();
+        unsafe {
+            tex.update_texture(&std::mem::transmute::<[u32;65536], [u8;262144]>(buffer));
+        }
+        tex
+    };
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::WHITE);
 
-        d.draw_texture(&tex1, 300, 300, Color::WHITE);
-        d.draw_texture_ex(&debug_tiles, math::Vector2::new(12., 12.), 0., 2., Color::WHITE);
+        d.draw_texture_ex(&tile_data, math::Vector2::new(800., 12.), 0., 2., Color::WHITE);
+        d.draw_texture(&bg_map, 800, 12 + 284, Color::WHITE);
+        d.draw_texture(&window, 800+256, 12 + 284, Color::WHITE);
     }
 }
